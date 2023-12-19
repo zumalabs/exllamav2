cd2d94f96b4891fae02381a4783e0176fb472063
Enrique Shockwave <33002121+qeternity@users.noreply.github.com>
2023-12-19 21:08:29 +0000

    imports

--- exllamav2/generator/__init__.py
+++ exllamav2/generator/__init__.py
@@ -3,4 +3,4 @@ from exllamav2.version import __version__
 from exllamav2.generator.sampler import ExLlamaV2Sampler
 from exllamav2.generator.base import ExLlamaV2BaseGenerator
 from exllamav2.generator.streaming import ExLlamaV2StreamingGenerator
-from exllamav2.generator.batched import ExLlamaV2BatchedGenerator, ExLlamaV2BatchedModel
+from exllamav2.generator.batched import ExLlamaV2BatchedGeneratorAsync, ExLlamaV2BatchedModel, ExLlamaV2BatchedModelAsync
